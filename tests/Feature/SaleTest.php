5e987c3e7f1d1ca2364b6b2bd1d2f0a3a134d705
Joao Scaleao <j.scaleao@hotmail.com>
2023-09-07 16:26:19 -0300

    add Test Feature Seller and Sale + Factor Seller

---
+++ tests/Feature/SaleTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Seller;
+use Illuminate\Support\Facades\Log;
+
+class SaleTest extends TestCase
+{
+    
+    use RefreshDatabase;
+    
+    public function test_sale_store(): void
+    {
+        $seller = Seller::factory()->create();
+        $amount = 1000;
+        $comission = $amount * 0.085;
+
+        $response = $this->postJson('/api/venda/', [
+                "seller_id" => $seller->id,
+                "amount" => $amount
+            ]);
+ 
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                "name" => $seller->name,
+                "email" => $seller->email,
+                "amount" => $amount,
+                "comission" => $comission
+
+            ]);
+    }
+
+    public function test_sale_show(): void
+    {
+        $seller = Seller::factory()->create();
+        $amount = 1000;
+
+        $response = $this->postJson('/api/venda/', [
+                "seller_id" => $seller->id,
+                "amount" => $amount
+            ]);
+        
+        $response = $this->getJson('/api/venda/'. $seller->id);
+
+        $response->assertStatus(200);
+    }
+}
---
+++ tests/Feature/SellerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Seller;
+
+class SellerTest extends TestCase
+{
+    
+    use RefreshDatabase;
+    
+    public function test_seller_index(): void
+    {
+        Seller::factory(5)->create();
+
+        $response = $this->getJson('/api/vendedores');
+ 
+        $response->assertStatus(200);
+    }
+
+    public function test_seller_store(): void
+    {
+        $response = $this->postJson('/api/vendedores/', [
+                "name" => "Scaleao",
+                "email" => "teste@teste.com"
+            ]);
+ 
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                "name" => "Scaleao",
+                "email" => "teste@teste.com"
+            ]);
+    }
+
+    public function test_seller_show(): void
+    {
+        $seller = Seller::factory()->create();
+ 
+        $response = $this->getJson('/api/vendedores/'. $seller->id);
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                "id" => $seller->id,
+                "name" => $seller->name,
+                "email" => $seller->email
+            ]);
+    }
+    
+    public function test_seller_delete(): void
+    {
+        $seller = Seller::factory()->create();
+ 
+        $response = $this->deleteJson('/api/vendedores/'. $seller->id);
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'Vendedor deletado com sucesso'
+            ]);
+    }
+}
