0c32151d09f6f7fcfb7cceabe0ad0045f23adfc3
Joao Scaleao <j.scaleao@hotmail.com>
2023-09-12 18:00:30 -0300

    satinize controller sale

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -10,14 +10,6 @@ use App\Http\Requests\SaleStoreRequest;
 class SaleController extends Controller
 {
     public function store(SaleStoreRequest $request){
-        // try{
-            // $sale = Sale::create($request->all());
-            // if($sale){
-            //     return response()->json($sale, 200);
-            // }
-        // } catch (\Exception $e) {
-        //     return response()->json(['erro' => 'Ocorreu um erro interno ao cadastrar venda'], 500);
-        // }
         return SaleService::store($request->validated());
     }
 
